61cb817775ba2b14878fdff83ee5d5544fcde452
rcamara <rcamara@settingconsultoria.com>
2015-11-02 16:17:17 +0100

    Menú mejorado para generación automatica con roles

--- resources/views/layouts/menu.blade.php
+++ resources/views/layouts/menu.blade.php
@@ -1,11 +1,11 @@
 <column cols="4" centered>
     <nav>
         <ul class="metismenu" id="side-menu">
-            <li><a href="">test <span class="fa arrow"></span></a> <span class="fa fa-user"></span></li>
-            <li><a href="">test <span class="fa arrow"></span></a> <span class="fa fa-user"></span></li>
-            <li><a href="">test <span class="fa arrow"></span></a> <span class="fa fa-user"></span></li>
-            <li><a href="">test <span class="fa arrow"></span></a> <span class="fa fa-user"></span></li>
-            <li><a href="">test <span class="fa arrow"></span></a> <span class="fa fa-user"></span></li>
+            <li><a href=""><span>test <span class="fa arrow"></span></span> <span class="fa fa-user"></span></a></li>
+            <li><a href=""><span>test <span class="fa arrow"></span></span> <span class="fa fa-user"></span></a></li>
+            <li><a href=""><span>test <span class="fa arrow"></span></span> <span class="fa fa-user"></span></a></li>
+            <li><a href=""><span>test <span class="fa arrow"></span></span> <span class="fa fa-user"></span></a></li>
+            <li><a href=""><span>test <span class="fa arrow"></span></span> <span class="fa fa-user"></span></a></li>
         </ul>
     </nav>
     <button class="navigation" type="primary"><span class="fa fa-navicon"></span></button>
