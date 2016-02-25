35d99a40e41ade1bd71e2cd1b499e481d9cade8d
Ricardo Cámara <rcamara9@gmail.com>
2016-02-25 19:24:47 +0100

    Fix

--- resources/lang/ca/messages.php
+++ resources/lang/ca/messages.php
@@ -29,6 +29,6 @@ return array(
     'tornar' => 'Tornar',
     'update_patient' => 'Actualitzar pacient',
     'update_user' => 'Actualitzar usuari',
-    'patient_update_correctly' => 'Pacient actualitzat correctament'
+    'patient_update_correctly' => 'Pacient actualitzat correctament',
     'user_blocked' => 'El teu usuari està bloquejat per què el sistema ha detectat massa intents amb dades errònies, contacta amb l\'administrador.'
 );
