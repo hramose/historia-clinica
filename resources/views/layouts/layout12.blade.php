ebfaa327e104ef8162b63d96b02cf57c851a83ca
rcamara <rcamara@settingconsultoria.com>
2016-03-09 16:03:14 +0100

    Mejora de Bower

--- resources/views/layouts/layout12.blade.php
+++ resources/views/layouts/layout12.blade.php
@@ -48,7 +48,7 @@
 <script src="https://cdnjs.cloudflare.com/ajax/libs/jquery/2.1.4/jquery.min.js"></script>
 <script src="https://ajax.googleapis.com/ajax/libs/angularjs/1.4.7/angular.min.js"></script>
 <script src="//ajax.googleapis.com/ajax/libs/angularjs/1.4.7/angular-animate.js"></script>
-{!! Html::script('../bower_components/angular-i18n/angular-locale_ca-es.js') !!}
+{!! Html::script('bower_components/angular-i18n/angular-locale_ca-es.js') !!}
 {!! Html::script('js/metismenu.js') !!}
 {!! Html::script('js/index.js') !!}
 {!! Html::script('js/app.js') !!}
