731f39e717416ecf7be2f6648a8730faae195e2d
rcamara <rcamara@settingconsultoria.com>
2016-03-10 09:35:43 +0100

    Fin debug, inicio remodelación Valoraciones

--- app/Console/Kernel.php
+++ app/Console/Kernel.php
@@ -28,6 +28,6 @@ class Kernel extends ConsoleKernel
     {
         /*$schedule->command('inspire')->everyMinute()->appendOutputTo(storage_path('logs/laravel.log'));*/
         $schedule->command('backup_db')->daily();
-        $schedule->command('empty_zip_dir')->everyMinute()->appendOutputTo(storage_path('logs/laravel.log'));
+        $schedule->command('empty_zip_dir')->daily()->appendOutputTo(storage_path('logs/laravel.log'));
     }
 }
