d0e1f55263b935cf129016e9d8504bafae9b25b1
rcamara <rcamara@settingconsultoria.com>
2016-03-10 09:28:56 +0100

    Arreglo importado log

--- app/Http/Controllers/BackupController.php
+++ app/Http/Controllers/BackupController.php
@@ -10,6 +10,7 @@ use Illuminate\Support\Facades\Auth;
 use Illuminate\Support\Facades\Crypt;
 use Illuminate\Support\Facades\DB;
 use Illuminate\Support\Facades\File;
+use Illuminate\Support\Facades\Log;
 use Illuminate\Support\Facades\Schema;
 use Illuminate\Support\Facades\Storage;
 use ZipArchive;
