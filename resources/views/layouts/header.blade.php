eb37c69571e0980e73989ca4e435f5e67ef504d7
rcamara <rcamara@settingconsultoria.com>
2016-04-01 09:20:10 +0200

    Para merge

--- resources/views/layouts/header.blade.php
+++ resources/views/layouts/header.blade.php
@@ -1,8 +1,6 @@
+<base href="{{ URL::to('/') }}">
 <link rel="shortcut icon" href="{{ asset('favicon.ico') }}">
-<link rel="stylesheet" href="{{ URL::asset('/css/kube.min.css') }}">
-<link rel="stylesheet" href="{{ URL::asset('/css/style.css') }}">
-<link rel="stylesheet" href="{{ URL::asset('/css/metismenu.css') }}">
-<link rel="stylesheet" href="{{ URL::asset('/css/animate.min.css') }}">
+<link rel="stylesheet" href="{{ URL::asset('/css/desktop.css') }}">
 <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/font-awesome/4.4.0/css/font-awesome.min.css">
 <script>
     var base_url = "{{ URL::to('/') }}";
