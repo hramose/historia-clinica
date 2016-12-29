4f9054f9777d8b298d3c9bff44a8d37bf56930c7
Ricardo Cámara <rcamara9@gmail.com>
2016-12-29 10:42:09 +0100

    arreglo ruta resetear contraseña

--- resources/lang/ca/auth.php
+++ resources/lang/ca/auth.php
@@ -13,7 +13,7 @@ return [
     |
     */
 
-    'failed' => 'Aquestes credencials no són correctes (potser l\'email o la contrasenya no estan ben escrits. Si no recorda la contrasenya podeu recuperar-la en el seguent enllaç: <a href="reset_password">Recuperar contrasenya</a>), no està registrat o el teu usuari no està verificat, si us plau, si és així adreceu-vos al mail que li vam enviar al seu moment per verificar el seu compte. Si no, pot sol·licitar de nou l\'email. <a href="sendverificationmail">Sol·licitar de nou</a>',
+    'failed' => 'Aquestes credencials no són correctes (potser l\'email o la contrasenya no estan ben escrits. Si no recorda la contrasenya podeu recuperar-la en el seguent enllaç: <a href="'.route('reset_password').'">Recuperar contrasenya</a>), no està registrat o el teu usuari no està verificat, si us plau, si és així adreceu-vos al mail que li vam enviar al seu moment per verificar el seu compte. Si no, pot sol·licitar de nou l\'email. <a href="sendverificationmail">Sol·licitar de nou</a>',
     'throttle' => 'Demasiados intentos de inicio de sesión. Por favor, inténtelo de nuevo en :seconds segundos',
 
 ];
